0eb359ebeffbb6f8c0e870eb9216ad939720e5f2
guikunz <130112411+guikunz@users.noreply.github.com>
2026-04-14 13:06:43 -0300

    app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def listar(lista): return "\n".join(lista) if lista else "Nenhum"
 # 1. CABEÇALHO DO SISTEMA CUSTOMIZADO
 st.markdown("""
     <div class='custom-header'>
-        <h1 style='margin-bottom: 0px;'>DEPARTAMENTO DE INVESTIGAÇÃO CRIMINAL - Supervidores</h1>
+        <h1 style='margin-bottom: 0px;'>DEPARTAMENTO DE INVESTIGAÇÃO CRIMINAL - Supervisores</h1>
         <p style='color: #a8a29e; font-size: 1.1rem; text-transform: uppercase; letter-spacing: 2px;'>Módulo de Conferência de graduação, patentes e cargos</p>
     </div>
 """, unsafe_allow_html=True)
